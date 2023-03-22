cbb6261a584df0545c0c5f82a2fd7d220dd9a450
Jack Sebben <jas8726@rit.edu>
2023-03-22 18:13:24 -0400

    Basic shopping cart functionality

--- Tour-of-Flags/src/app/shopping-cart/shopping-cart.component.ts
+++ Tour-of-Flags/src/app/shopping-cart/shopping-cart.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
 
+import { Account } from '../account';
+import { ShoppingCart } from '../shopping-cart';
+import { AccountService } from '../account.service';
+import { Flag } from '../flag';
+import { FlagService } from '../flag.service';
+
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css']
 })
 export class ShoppingCartComponent {
+  currentAccount!: Account;
+  cart: ShoppingCart | undefined;
 
+  constructor(private accountService: AccountService, private flagService: FlagService) {
+    if (accountService.getCurrentAccount()) {
+      this.currentAccount = accountService.getCurrentAccount()!;
+    }
+   }
+
+  ngOnInit(): void {
+    this.getCart();
+  }
+
+  getCart(): void {
+    if (this.currentAccount) {
+      this.accountService.getCart(this.currentAccount.username)
+      .subscribe(cart => this.cart = cart);
+    }
+  }
+
+  getFlagFromID(id: number): any {
+    // Need to figure this out
+  }
+
+  add(id: number): void {
+    this.accountService.addFlagCart(this.currentAccount.username, id)
+      .subscribe(() => {
+        this.getCart();
+      });
+  }
+
+  delete(id: number): void {
+    this.accountService.deleteFlagCart(this.currentAccount.username, id)
+      .subscribe(() => {
+        this.getCart();
+      });
+  }
 }

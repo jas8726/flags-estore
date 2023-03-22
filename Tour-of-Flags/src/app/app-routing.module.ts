16b090f8396d8e9aedb4c7e8b0fe1985e609d4df
Jack Sebben <jas8726@rit.edu>
2023-03-21 23:50:16 -0400

    Blank shopping cart component w/ dashboard button
    And rearranged some of the dashboard buttons/layout

--- Tour-of-Flags/src/app/app-routing.module.ts
+++ Tour-of-Flags/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { FlagsComponent } from './flags/flags.component';
 import { FlagDetailComponent } from './flag-detail/flag-detail.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -13,7 +14,8 @@ const routes: Routes = [
   { path: 'detail/:id', component: FlagDetailComponent },
   { path: 'flags', component: FlagsComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'shoppingCart', component: ShoppingCartComponent }
 ];
 
 @NgModule({

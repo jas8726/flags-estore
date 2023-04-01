b4639711ff4af2bc1e74711ed680d3cfde5b45ed
Jack Sebben <jas8726@rit.edu>
2023-04-01 16:12:57 -0400

    Flags can be checked out successfully

--- Tour-of-Flags/src/app/shopping-cart/shopping-cart.component.ts
+++ Tour-of-Flags/src/app/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { Account } from '../account';
 import { CartItem } from '../cart-item';
@@ -14,8 +15,11 @@ import { FlagService } from '../flag.service';
 export class ShoppingCartComponent implements OnInit {
   currentAccount?: Account;
   cart: CartItem[] = [];
+  errorText: String = "";
 
-  constructor(private accountService: AccountService, private flagService: FlagService) {
+  constructor(
+    private accountService: AccountService,
+    private flagService: FlagService) {
     this.currentAccount = accountService.getCurrentAccount();
   }
 
@@ -50,5 +54,36 @@ export class ShoppingCartComponent implements OnInit {
         this.getCart();
       });
     }
+  }
+
+  checkout(): void {
+    this.cart.forEach(cartItem => {
+      var flag = this.getFlagFromID(cartItem);
+      if (!flag) {
+        this.errorText = "One of your flags does not exist!";
+        return;
+      }
+      var newQuantity = flag.quantity - cartItem.quantity;
+      if (newQuantity < 0) {
+        this.errorText = "You are trying to buy " + cartItem.quantity + " " 
+        + flag.name + " when there are only " + flag.quantity + " available.";
+        return
+      }
+      flag.quantity = newQuantity;
+    });
+
+    this.errorText = "";
+
+    this.cart.forEach(purchase => {
+      this.flagService.updateFlag(this.getFlagFromID(purchase)!)
+        .subscribe(() => {
+          for (let index = 0; index < purchase.quantity; index++) {
+            this.delete(purchase.flagID);
+          }
+        });
+    });
+    
+    // GOTO CHECKOUT SCREEN
+    this.errorText = "Checked out flags!"
   }
 }

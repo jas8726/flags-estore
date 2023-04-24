1d71e85584d62054a895af1542679a8102f44b1d
Oliver Vinneras <ov5800@rit.edu>
2023-04-24 14:37:50 -0400

    Tour of Flags to Flag Store

--- Tour-of-Flags/src/app/app.component.ts
+++ Tour-of-Flags/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { AccountService } from './account.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of Flags';
+  title = 'Flag Store';
 
   constructor(
     public accountService: AccountService
